6b5044d423c9d7709662fdd6102587ae47dabf14
21.11.4346_Ahmad <ahmadfauzan.280.af@gmail.com>
2024-06-15 20:20:01 +0700

    update style font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/lib/esm/**/*.js"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"], // Add Manrope to font family
+      },
       screens: {
         mobile: "0px",
         tablet: "425px",
